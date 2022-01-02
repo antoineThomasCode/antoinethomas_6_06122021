03a813b741207c427cc16c0a89651abdefad2017
antoineThomasCode <73194840+antoineThomasCode@users.noreply.github.com>
2022-01-02 13:51:48 +0100

    fixed issue in path for photographer page sorting

--- scripts/factories/medias.js
+++ scripts/factories/medias.js
@@ -1,211 +1,202 @@
-
 // ici faire une factory media sur le modèle de la factory photographer
 function mediaFactory(data) {
-   
-    const { photographerId, id, image, video,  title, date, price, likes } = data;
-   
+  const { photographerId, id, image, video, title, date, price, likes } = data
 
-    const mediaPathImg = `../assets/images/${photographerInfos.name}/${image}`
-    const mediaPathVideo = `../assets/images/${photographerInfos.name}/${video}`
-    let path = ""
-    let mediaInfosLightbox = document.createElement('div');
-    const article = document.createElement('article');
-   
+  const mediaPathImg = `../assets/images/${photographerInfos.name}/${image}`
+  const mediaPathVideo = `../assets/images/${photographerInfos.name}/${video}`
+  let path = ''
+  let mediaInfosLightbox = document.createElement('div')
+  const article = document.createElement('article')
 
-    // *** tools to display medias in photographers' portfolio *** //
-    function createImage() {
-        //setting image
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", mediaPathImg);
-        img.setAttribute("alt", `${title} `);
-        path = mediaPathImg
+  // *** tools to display medias in photographers' portfolio *** //
+  function createImage() {
+    //setting image
+    const img = document.createElement('img')
+    img.setAttribute('src', mediaPathImg)
+    img.setAttribute('alt', `${title} `)
+    path = mediaPathImg
 
-        // insert items in articles
-        article.appendChild(img)
-        img.addEventListener('click', createLightbox)
-        srcForGallery.push(mediaPathImg)
+    // insert items in articles
+    article.appendChild(img)
+    img.addEventListener('click', createLightbox)
+    srcForGallery.push(mediaPathImg)
+  }
+  function createVideo() {
+    // create video  and setting path
+    const video = document.createElement('video')
+    video.setAttribute('src', mediaPathVideo)
+    video.setAttribute('alt', `${title}`)
+    video.setAttribute('type', 'video/mp4')
+    video.setAttribute('loop', '')
+    video.setAttribute('muted', '')
+    video.addEventListener('click', createLightbox)
+
+    path = mediaPathVideo
+    srcForGallery.push(mediaPathVideo)
+
+    // insert video in article
+    article.appendChild(video)
+  }
+  function getMediaItems() {
+    // select portfolio items container
+    const section = document.getElementById('mediaContainer')
+
+    // create and setting items
+    // container for image, like and title
+
+    const infosItemsContainer = document.createElement('div')
+    const h3 = document.createElement('h3')
+    const likesPerItem = document.createElement('span')
+    const likeIcon = document.createElement('i')
+    let isLiked = false
+
+    // setting title
+    h3.textContent = title
+
+    likesPerItem.textContent = likes
+    likesPerItem.style.cursor = 'pointer'
+    likesPerItem.appendChild(likeIcon)
+    likeIcon.classList.add('fas')
+    likeIcon.classList.add('fa-heart')
+    titlesForGallery.push(title)
+    // create image or video
+    if (image) {
+      // create image and setting path
+      createImage()
+    } else {
+      createVideo()
     }
-    function createVideo() {
-        // create video  and setting path 
-        const video = document.createElement('video');
-        video.setAttribute("src", mediaPathVideo);
-        video.setAttribute("alt", `${title}`);
-        video.setAttribute("type", "video/mp4");
-        video.setAttribute("loop", "")
-        video.setAttribute("muted", "")
-        video.addEventListener('click', createLightbox)
-
-        path = mediaPathVideo
-        srcForGallery.push(mediaPathVideo)
-
-        // insert video in article 
-        article.appendChild(video) 
-      
-    }
-    function getMediaItems() {
-        // select portfolio items container 
-        const section = document.getElementById('mediaContainer');
-
-        // create and setting items 
-        // container for image, like and title 
- 
-
-        const infosItemsContainer = document.createElement('div');
-        const h3 = document.createElement('h3'); 
-        const likesPerItem = document.createElement('span'); 
-        const likeIcon = document.createElement('i')
-        let isLiked = false; 
-       
-        // setting title
-        h3.textContent = title; 
-        
-        likesPerItem.textContent = likes; 
-        likesPerItem.style.cursor = "pointer"
+    infosItemsContainer.addEventListener('click', function (e) {
+      if (!isLiked) {
+        isLiked = true
+        likesPerItem.textContent = likes + 1
         likesPerItem.appendChild(likeIcon)
-        likeIcon.classList.add('fas')
-        likeIcon.classList.add('fa-heart') 
-        titlesForGallery.push(title)
-        // create image or video 
-        if (image) {
-            // create image and setting path
-           createImage();
+      }
+    })
+    //insert items in section container
 
-        } else {
-            createVideo();
-        }
-        infosItemsContainer.addEventListener('click', function(e){
-            if (!isLiked) {
-                isLiked = true; 
-                likesPerItem.textContent = likes + 1;
-                likesPerItem.appendChild(likeIcon)
-            }
-        })
-        //insert items in section container 
+    article.append(infosItemsContainer)
+    infosItemsContainer.append(h3, likesPerItem)
+    section.appendChild(article)
+  }
+  // ** tools to display media in the lightbox //
 
-        article.append(infosItemsContainer)
-        infosItemsContainer.append(h3, likesPerItem)
-        section.appendChild(article)
+  // Lightbox creation //
+  function createLightbox() {
+    const header = document.getElementById('main')
+    let mediaToDisplay
+    let infosMedia = document.createElement('div')
+    const h2 = document.createElement('h2')
+    h2.id = 'lightbox-title'
+    h2.textContent = title
+    infosMedia.appendChild(h2)
+    const btnClose = document.createElement('button')
+    btnClose.innerHTML = '<i class="fas fa-times"></i>'
+
+    // create Next Button in the lightbox
+    const btnNext = document.createElement('i')
+    btnNext.id = 'btn-next'
+    btnNext.classList.add('fas', 'fa-chevron-right')
+    // button Next Event
+    const buttonNextLightbox = () => {
+      let regex = /\.jpg/i
+      let newMedia
+      let index = titlesForGallery.indexOf(h2.innerHTML)
+      let src
+      containerMedia.removeChild(mediaToDisplay)
+      if (index === titlesForGallery.length - 1) {
+        index = 0
+      } else {
+        index++
+      }
+      h2.textContent = titlesForGallery[index]
+      if (regex.test(srcForGallery[index])) {
+        newMedia = document.createElement('img')
+        newMedia.setAttribute('src', srcForGallery[index])
+        newMedia.setAttribute('alt', titlesForGallery[index])
+      } else {
+        newMedia = document.createElement('video')
+        newMedia.setAttribute('src', srcForGallery[index])
+        newMedia.setAttribute('alt', titlesForGallery[index])
+        newMedia.setAttribute('type', 'video/mp4')
+        newMedia.setAttribute('autoplay', 'autoplay')
+        newMedia.setAttribute('preload', 'auto')
+        newMedia.setAttribute('controls', '')
+        newMedia.setAttribute('loop', '')
+      }
+      mediaToDisplay = newMedia
+      containerMedia.appendChild(mediaToDisplay)
     }
-    // ** tools to display media in the lightbox //
+    // create button Prev in the Lightbox
 
-    // Lightbox creation //
-    function createLightbox() {
+    const btnPrev = document.createElement('i')
+    btnPrev.classList.add('fas', 'fa-chevron-left')
+    btnPrev.id = 'btn-prev'
 
-        const header = document.getElementById('main')
-        let mediaToDisplay; 
-        let infosMedia = document.createElement('div')
-        const h2 = document.createElement("h2")
-        h2.id = 'lightbox-title'
-        h2.textContent = title
-        infosMedia.appendChild(h2)
-        const btnClose = document.createElement('button')
-        btnClose.innerHTML = '<i class="fas fa-times"></i>'
-        const btnPrev = document.createElement('i')
-        btnPrev.classList.add('fas', 'fa-chevron-left')
-        btnPrev.id = 'btn-prev'
+    // button Prev Event
 
-        // create Next Button in the lightbox
-        const btnNext = document.createElement('i')
-        btnNext.id = 'btn-next'
-        btnNext.classList.add('fas', 'fa-chevron-right')
-        // create image --> OR vidéo
-        if (image) {
-            mediaToDisplay = document.createElement('img')
-        } else {
-            mediaToDisplay = document.createElement('video')
-            mediaToDisplay.setAttribute("alt", `${title}`);
-            mediaToDisplay.setAttribute("type", "video/mp4");
-            mediaToDisplay.setAttribute("autoplay", "autoplay")
-            mediaToDisplay.setAttribute("preload", "auto")
-            mediaToDisplay.setAttribute("controls", "")
-            mediaToDisplay.setAttribute("loop", "")
-        }
-        mediaToDisplay.id = "mediaDisplayed"
-        mediaToDisplay.setAttribute('src', path)
-
-        // Display media in lightbox
-        const lightbox = document.createElement('div')
-        const containerMedia = document.createElement('div')
-        containerMedia.id = 'container-media'
-        lightbox.id = 'lightbox'
-        header.append(lightbox)
-
-        lightbox.appendChild(containerMedia)
-        containerMedia.appendChild(mediaToDisplay)
-
-        lightbox.appendChild(infosMedia)
-
-        btnClose.addEventListener('click', function(){
-            header.removeChild(lightbox)
-            })
-        btnNext.addEventListener('click', function(){
-            
-            let regex = /\.jpg/i;
-            let newMedia;
-            let index = titlesForGallery.indexOf(h2.innerHTML)
-            let src
-            containerMedia.removeChild(mediaToDisplay)
-            if (index === (titlesForGallery.length - 1)) {
-                index = 0
-            } else {
-                  index++
-            }
-            h2.textContent = titlesForGallery[index]
-            if(regex.test(srcForGallery[index])) {
-                newMedia = document.createElement('img')
-                newMedia.setAttribute('src', srcForGallery[index])
-                newMedia.setAttribute("alt", titlesForGallery[index]);
-
-            } else {
-                newMedia = document.createElement('video')
-                newMedia.setAttribute('src', srcForGallery[index])
-                newMedia.setAttribute("alt", titlesForGallery[index]);
-                newMedia.setAttribute("type", "video/mp4");
-                newMedia.setAttribute("autoplay", "autoplay")
-                newMedia.setAttribute("preload", "auto")
-                newMedia.setAttribute("controls", "")
-                newMedia.setAttribute("loop", "")
-            }
-            mediaToDisplay = newMedia
-            containerMedia.appendChild(mediaToDisplay)
-            
-        })    
-        btnPrev.addEventListener('click', function(){
-            
-            let regex = /\.jpg/i;
-            let newMedia;
-            let index = titlesForGallery.indexOf(h2.innerHTML)
-            let src
-            containerMedia.removeChild(mediaToDisplay)
-            if (index === 0) {
-                index = titlesForGallery.length - 1
-            } else {
-                  index--
-            }
-            h2.textContent = titlesForGallery[index]
-            if(regex.test(srcForGallery[index])) {
-                newMedia = document.createElement('img')
-                newMedia.setAttribute('src', srcForGallery[index])
-                newMedia.setAttribute("alt", titlesForGallery[index]);
-
-            } else {
-                newMedia = document.createElement('video')
-                newMedia.setAttribute('src', srcForGallery[index])
-                newMedia.setAttribute("alt", titlesForGallery[index]);
-                newMedia.setAttribute("type", "video/mp4");
-                newMedia.setAttribute("autoplay", "autoplay")
-                newMedia.setAttribute("preload", "auto")
-                newMedia.setAttribute("controls", "")
-                newMedia.setAttribute("loop", "")
-            }
-            mediaToDisplay = newMedia
-            containerMedia.appendChild(mediaToDisplay)
-            
-        })  
-        lightbox.append(btnClose ,btnPrev, btnNext)
-        
+    const buttonPrevLigthbox = () => {
+      let regex = /\.jpg/i
+      let newMedia
+      let index = titlesForGallery.indexOf(h2.innerHTML)
+      let src
+      containerMedia.removeChild(mediaToDisplay)
+      if (index === 0) {
+        index = titlesForGallery.length - 1
+      } else {
+        index--
+      }
+      h2.textContent = titlesForGallery[index]
+      if (regex.test(srcForGallery[index])) {
+        newMedia = document.createElement('img')
+        newMedia.setAttribute('src', srcForGallery[index])
+        newMedia.setAttribute('alt', titlesForGallery[index])
+      } else {
+        newMedia = document.createElement('video')
+        newMedia.setAttribute('src', srcForGallery[index])
+        newMedia.setAttribute('alt', titlesForGallery[index])
+        newMedia.setAttribute('type', 'video/mp4')
+        newMedia.setAttribute('autoplay', 'autoplay')
+        newMedia.setAttribute('preload', 'auto')
+        newMedia.setAttribute('controls', '')
+        newMedia.setAttribute('loop', '')
+      }
+      mediaToDisplay = newMedia
+      containerMedia.appendChild(mediaToDisplay)
     }
+    // create image --> OR vidéo
+    if (image) {
+      mediaToDisplay = document.createElement('img')
+    } else {
+      mediaToDisplay = document.createElement('video')
+      mediaToDisplay.setAttribute('alt', `${title}`)
+      mediaToDisplay.setAttribute('type', 'video/mp4')
+      mediaToDisplay.setAttribute('autoplay', 'autoplay')
+      mediaToDisplay.setAttribute('preload', 'auto')
+      mediaToDisplay.setAttribute('controls', '')
+      mediaToDisplay.setAttribute('loop', '')
+    }
+    mediaToDisplay.id = 'mediaDisplayed'
+    mediaToDisplay.setAttribute('src', path)
 
-    
-    return { getMediaItems, createLightbox}
+    // Display media in lightbox
+    const lightbox = document.createElement('div')
+    const containerMedia = document.createElement('div')
+    containerMedia.id = 'container-media'
+    lightbox.id = 'lightbox'
+    header.append(lightbox)
+
+    lightbox.appendChild(containerMedia)
+    containerMedia.appendChild(mediaToDisplay)
+
+    lightbox.appendChild(infosMedia)
+
+    btnClose.addEventListener('click', function () {
+      header.removeChild(lightbox)
+    })
+    btnNext.addEventListener('click', buttonNextLightbox)
+    btnPrev.addEventListener('click', buttonPrevLigthbox)
+    lightbox.append(btnClose, btnPrev, btnNext)
+  }
+  return { getMediaItems, createLightbox }
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,32 +1,32 @@
 // catch id in URL
 let params = new URL(document.location).searchParams
 const photographerId = params.get('id')
-let userLikesCounter = 0; 
-
+let userLikesCounter = 0
 
 const srcForGallery = []
 const titlesForGallery = []
 
-// connect to json file 
+// connect to json file
 async function getPhotographerInfos() {
   try {
     const response = await fetch('../data/photographers.json')
-    
+
     return await response.json()
   } catch {
     console.log('erreur de la requête')
   }
 }
-// display items in page or console log an error 
+// display items in page or console log an error
 async function displayPortfolioItems(photographers) {
-  //catch photographer array 
+  //catch photographer array
 
   if (photographerId) {
-
-    //display items in header 
-    photographerInfos = photographers.find((photographer) => photographer.id == photographerId)
-    const photographerModel = photographerFactory(photographerInfos);
-    const userInfosElements = photographerModel.getPortoflioItems();
+    //display items in header
+    photographerInfos = photographers.find(
+      (photographer) => photographer.id == photographerId,
+    )
+    const photographerModel = photographerFactory(photographerInfos)
+    const userInfosElements = photographerModel.getPortoflioItems()
     return photographerInfos.name
   } else {
     console.log(
@@ -35,72 +35,72 @@ async function displayPortfolioItems(photographers) {
   }
 }
 async function displayDataUser(medias) {
-    const url = new URL(document.location)
-    const portfolioMain = document.getElementById("main");
-    const section = document.createElement( 'section' );
-    const sorterContainer = document.createElement('div')
-    const p = document.createElement('p')
-    const ul = document.createElement('ul')
-    const byPopularity = document.createElement('li')
-    const byTitle = document.createElement('li')
+  const url = new URL(document.location)
+  const portfolioMain = document.getElementById('main')
+  const section = document.createElement('section')
+  const sorterContainer = document.createElement('div')
+  const p = document.createElement('p')
+  const ul = document.createElement('ul')
+  const byPopularity = document.createElement('li')
+  const byTitle = document.createElement('li')
 
-    section.id = 'mediaContainer';
-    // create sorter for media display --> using URL  params 
-    sorterContainer.id = 'sorter'
-    p.textContent = 'Trier par'
-    byPopularity.innerHTML = `<a href="${url.origin}/pages/photographer.html?id=${photographerId}&sortBy=popularity">Popularité <i class="fas fa-chevron-right"></i></a>`
-    byTitle.innerHTML = `<a href="${url.origin}/pages/photographer.html?id=${photographerId}&sortBy=title">Titre <i class="fas fa-chevron-right"></i></a>`
+  section.id = 'mediaContainer'
+  // create sorter for media display --> using URL  params
+  sorterContainer.id = 'sorter'
+  p.textContent = 'Trier par'
+  byPopularity.innerHTML = `<a href="${url.origin}/${url.pathname}?id=${photographerId}&sortBy=popularity">Popularité <i class="fas fa-chevron-right"></i></a>`
+  byTitle.innerHTML = `<a href="${url.origin}/${url.pathname}?id=${photographerId}&sortBy=title">Titre <i class="fas fa-chevron-right"></i></a>`
 
-    //console.log(url)
-    
-    portfolioMain.appendChild(section)
-    mediaContainer.appendChild(sorterContainer)
-    sorterContainer.append(p, ul)
-    ul.append(byPopularity, byTitle)
-    if (url.searchParams.get('sortBy') == 'popularity') {
-      medias = medias.sort(function (a, b) {
-        return b.likes - a.likes
-      })
+  console.log(url)
+
+  portfolioMain.appendChild(section)
+  mediaContainer.appendChild(sorterContainer)
+  sorterContainer.append(p, ul)
+  ul.append(byPopularity, byTitle)
+  if (url.searchParams.get('sortBy') == 'popularity') {
+    medias = medias.sort(function (a, b) {
+      return b.likes - a.likes
+    })
+  }
+  if (url.searchParams.get('sortBy') == 'title') {
+    medias = medias.sort(function SortArray(x, y) {
+      if (x.title < y.title) {
+        return -1
+      }
+      if (x.title > y.title) {
+        return 1
+      }
+      return 0
+    })
+  }
+  // display media with media factory
+  medias.forEach((media) => {
+    if (media.photographerId == photographerId) {
+      let mediaModel = mediaFactory(media)
+      const userDataDOM = mediaModel.getMediaItems()
     }
-    if (url.searchParams.get('sortBy') == 'title') {
-      medias = medias.sort(function SortArray(x, y){
-        if (x.title < y.title) {return -1;}
-        if (x.title > y.title) {return 1;}
-        return 0;
-      })
-    }
-    // display media with media factory 
-    medias.forEach((media) => {
-        if (media.photographerId == photographerId){
-          let mediaModel = mediaFactory(media);
-          const userDataDOM = mediaModel.getMediaItems();
-        }
-
-    });
-   
+  })
 }
 async function displayLikes(medias) {
-
-    const stickyUserInfos = document.getElementById('sticky-user-infos')
-    const userLikes = document.createElement('span');
-    medias.forEach((media) => {
-      if (media.photographerId == photographerId){
-        userLikesCounter += media.likes; 
-      }
-    userLikes.textContent = userLikesCounter;
-    const heartIcon = document.createElement('i');
+  const stickyUserInfos = document.getElementById('sticky-user-infos')
+  const userLikes = document.createElement('span')
+  medias.forEach((media) => {
+    if (media.photographerId == photographerId) {
+      userLikesCounter += media.likes
+    }
+    userLikes.textContent = userLikesCounter
+    const heartIcon = document.createElement('i')
     heartIcon.classList.add('fas')
-    heartIcon.classList.add('fa-heart') 
+    heartIcon.classList.add('fa-heart')
     userLikes.appendChild(heartIcon)
-    stickyUserInfos.appendChild(userLikes);
+    stickyUserInfos.appendChild(userLikes)
     const formTitle = document.getElementById('formTitle')
     formTitle.innerHTML = `Contactez-moi ${photographerInfos.name}`
   })
 }
 
-// call functions to display data in page 
+// call functions to display data in page
 async function init() {
-
   const { photographers, medias } = await getPhotographerInfos()
   displayPortfolioItems(photographers)
   displayDataUser(medias)
