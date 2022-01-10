1fa703121d310209c83397acace6d9d8637d6afe
Antoine Thomas <antoinethomas.devweb@gmail.com>
2022-01-10 12:38:08 +0100

    webpack config

---
+++ src/pages/homepage.js
@@ -0,0 +1,28 @@
+async function getPhotographers() {
+    try {
+        const response = await fetch('./data/photographers.json');
+        return await response.json();
+        
+    } catch {
+       console.log('erreur de la requête')
+    }
+}
+
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
+
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        const photographerHeader = document.querySelector('.photograph-header')
+        photographersSection.appendChild(userCardDOM);
+    });
+};
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
+};
+
+init();
