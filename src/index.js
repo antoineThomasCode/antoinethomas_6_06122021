ca1affe768b40bd296072293e7e0360ab47a5ebb
Antoine Thomas <antoinethomas.devweb@gmail.com>
2022-01-17 11:13:19 +0100

    idem

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ const url = new URL(document.location).pathname
 
 
 if (url === '/antoinethomas_6_06122021/pages/photographer.html') {
+    console.log(url)
     initPortfolio()
 } else {
     initHomePage()
+    console.log(url)
 }
   
 
