2f181870f052ab4ef71fa2bed6013dffaeba29ff
Antoine Thomas <antoinethomas.devweb@gmail.com>
2022-01-17 11:16:42 +0100

    idem

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import initHomePage from '../src/pages/homepage'
 import { initPortfolio} from '../src/pages/photographer'
 
 const url = new URL(document.location).pathname
-
+console.log(url)
 
 
 if (url === '/antoinethomas_6_06122021/pages/photographer.html') {
-    console.log(url)
+    
     initPortfolio()
 } else {
     initHomePage()
